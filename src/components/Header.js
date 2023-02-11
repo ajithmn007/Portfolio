62a6dd0cf55beb6045040596736dd67f524ee759
senthamil <senthamil.selven2612@gmail.com>
2023-02-11 22:32:11 +0530

    added sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,45 @@
-import React from "react";
-import styles from "./Header.module.css";
-import { useScrollDirection } from "../Assets/useScrollDirection";
+import React, { useState } from 'react';
+import styles from './Header.module.css';
+import { useScrollDirection } from '../Assets/useScrollDirection';
+import { Waypoint } from 'react-waypoint';
 export default function Header() {
-  const scroll = useScrollDirection();
-  console.log(scroll);
-  const header = `${styles.header} ${
-    scroll === "down" ? styles.hide : styles.show
-  }`;
-  console.log(header);
-  return (
-    <>
-      <header className={header}>
-        <nav className={styles.nav}>
-          <h5 className={styles.name}>Ajith</h5>
-          <div className={styles["main-nav"]}>
-            <ul className={styles.ul}>
-              <li className={styles.li}>
-                <a href="/">Home</a>
-              </li>
-              <li className={styles.li}>
-                <a href="#about">About</a>
-              </li>
-              <li className={styles.li}>
-                <a href="#skills">Skills</a>
-              </li>
-              <li className={styles.li}>
-                <a href="#projects">Projects</a>
-              </li>
-              <li className={styles.li}>
-                <a href="#contact">Contact</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
+	const scroll = useScrollDirection();
+	const [isStickyHeader, setIsStickyHeader] = useState(false);
+	console.log(scroll);
+	const header = `${styles.header} ${
+		scroll === 'down' ? styles.hide : styles.show
+	}`;
+	console.log(header);
+	return (
+		<Waypoint
+			onEnter={() => setIsStickyHeader(false)}
+			onLeave={() => setIsStickyHeader(true)}
+      bottomOffset='60px'
+		>
+			<header className={header}>
+				<nav className={`${styles.nav} ${isStickyHeader? styles.sticky:''}`}>
+					<h5 className={styles.name}>Ajith</h5>
+					<div className={styles['main-nav']}>
+						<ul className={styles.ul}>
+							<li className={styles.li}>
+								<a href="/">Home</a>
+							</li>
+							<li className={styles.li}>
+								<a href="#about">About</a>
+							</li>
+							<li className={styles.li}>
+								<a href="#skills">Skills</a>
+							</li>
+							<li className={styles.li}>
+								<a href="#projects">Projects</a>
+							</li>
+							<li className={styles.li}>
+								<a href="#contact">Contact</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+		</Waypoint>
+	);
 }
